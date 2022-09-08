51c472d8ab68574f102e5a1d5b6a8dc4a065574d
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-09-08 13:24:28 -0700

    Implemented the push and pall opcodes

---
+++ opcodes_1.c
@@ -0,0 +1,77 @@
+#include "monty.h"
+
+/**
+* monty_push - Pushes an element to the stack
+* @stack: pointer
+* @line_number: current working line no of monty bytecodes file
+*/
+
+void monty_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp, *new;
+	int i;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		set_op_tok_error(malloc_error());
+		return;
+	}
+
+	if (op_toks[1] == NULL)
+	{
+		set_op_tok_error(no_int_error(line_number));
+		return;
+	}
+
+	for (i = 0; op_toks[1][i]; i++)
+	{
+		if (op_toks[1][i] == '-' && i == 0)
+			continue;
+		if (op_toks[1][i] < '0' || op_toks[1][i] > '9')
+		{
+			set_op_tok_error(no_int_error(line_number));
+			return;
+		}
+	}
+	new->n = atoi(op_toks[1]);
+
+	if (check_mode(*stack) == STACK)
+	{
+		tmp = (*stack)->next;
+		new->prev = *stack;
+		new->next = tmp;
+		if (tmp)
+			tmp->prev = new;
+		(*stack)->next = new;
+	}
+	else
+	{
+		tmp = *stack;
+		while (tmp->next)
+			tmp = tmp->next;
+		new->prev = tmp;
+		new->next = NULL;
+		tmp->next = new;
+	}
+}
+
+
+
+/**
+* monty_pall - prints all the values on the stack, starting from the top of the stack
+* @stack:pointer
+* @line_number:current working line no of a monty bytecode file
+*/
+
+void monty_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+	(void)line_number;
+}
